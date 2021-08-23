bb4238f5e625ea8bf6ef35fc6c6baec5cbd405bf
Luis Galindo <egalindo54@uabc.edu.mx>
2021-08-23 00:24:35 -0600

    Ahora se puede ajustar el color de la titlebar con js

--- publish/script.js
+++ publish/script.js
@@ -11,3 +11,28 @@ if( theme ) {
 
 localStorage.setItem('theme', 'light');
 body.classList.value =  'light' ;
+
+var metaThemeColor = document.querySelector("meta[name=theme-color]");
+metaThemeColor.setAttribute("content", "#eeeeee");
+
+
+// function getRandomRGBValue() {
+//     return Math.min(Math.floor(Math.random() * 255 + 1), 255);
+// }
+
+// function getRandomColor() {
+//     var r = getRandomRGBValue(),
+//         g = getRandomRGBValue(),
+//         b = getRandomRGBValue();
+//     return "#" + (((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1));
+// }
+
+// function changeThemeColor() {
+//     var metaThemeColor = document.querySelector("meta[name=theme-color]");
+//     metaThemeColor.setAttribute("content", getRandomColor());
+//     setTimeout(function() {
+//         changeThemeColor();
+//     }, 3000);
+// }
+
+// changeThemeColor();
