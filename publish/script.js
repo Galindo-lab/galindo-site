f49b25c6e101677f033c2588dbe8cc18da2551ad
Luis Galindo <egalindo54@uabc.edu.mx>
2021-12-04 16:59:50 -0700

    sáb 04 dic 2021 16:59:54

--- publish/script.js
+++ publish/script.js
@@ -1,51 +1,22 @@
+window.onload = () => {
+    let body = document.body
 
-const body = document.body
-const theme = localStorage.getItem("theme")
-
-if( theme ) {
-    body.classList.value = theme 
-} else {
-    localStorage.setItem('theme', 'light');
-    body.classList.value =  'light' 
-}
-
-// localStorage.setItem('theme', 'light');
-// body.classList.value =  'light' ;
-
-var metaThemeColor = document.querySelector("meta[name=theme-color]");
-metaThemeColor.setAttribute("content", (theme==="dark" ? "#333333" : "#eeeeee") );
-
+    if( localStorage.getItem("theme") ) {
+        body.classList.value = localStorage.getItem("theme")
+    } else {
+        localStorage.setItem('theme', 'light');
+        body.classList.value =  'light' 
+    }
+};
 
 function switchTheme() {
-   if( localStorage.getItem("theme") == 'dark' ){
-       localStorage.setItem("theme", 'light')
-       metaThemeColor.setAttribute("content", "#eeeeee");
-   } else {
-       localStorage.setItem("theme", 'dark')
-       metaThemeColor.setAttribute("content", "#333333");
-   }
+    let body = document.body;
+    
+    if( localStorage.getItem("theme") == 'dark' ){
+        localStorage.setItem("theme", 'light')
+    } else {
+        localStorage.setItem("theme", 'dark')
+    }
 
-   body.classList.value =  localStorage.getItem("theme");
+    body.classList.value =  localStorage.getItem("theme");
 }
-
-
-// function getRandomRGBValue() {
-//     return Math.min(Math.floor(Math.random() * 255 + 1), 255);
-// }
-
-// function getRandomColor() {
-//     var r = getRandomRGBValue(),
-//         g = getRandomRGBValue(),
-//         b = getRandomRGBValue();
-//     return "#" + (((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1));
-// }
-
-// function changeThemeColor() {
-//     var metaThemeColor = document.querySelector("meta[name=theme-color]");
-//     metaThemeColor.setAttribute("content", getRandomColor());
-//     setTimeout(function() {
-//         changeThemeColor();
-//     }, 3000);
-// }
-
-// changeThemeColor();
